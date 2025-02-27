9dd55b614752839efd5d355a19c06d4dea87595e
Kanav Guleria <Kanav.Guleria@Triconinfotech.com>
2025-02-27 15:34:43 +0530

    added url creator form

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Details from './home.details'
 import { Outlet } from '@remix-run/react'
+import UrlCreator from "../../src/components/UrlCreator"
 
 function Home() {
+    const [url,setUrl] = useState("");
+
+    const onUrlChangeHandler = (value) => {
+        console.log(value);
+        setUrl(value);
+    };
+    
     return (
         <div>
-            Home..............................
+            <UrlCreator onUrlChangeHandler={onUrlChangeHandler} />
+            <span>URL: {url}</span>
             <Outlet />
         </div>
-    )
+    );
 }
 
 export default Home
