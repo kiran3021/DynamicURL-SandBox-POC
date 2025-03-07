fedeb5bca9ba475534b01595ce8bb768112f5317
Madhu Kiran Reddy <Madhu.Reddy@Triconinfotech.com>
2025-03-07 19:35:05 +0530

    created sitemap

---
+++ app/routes/sitemap.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sitemap from 'src/components/sitemap/Sitemap'
+import HandleExpansionDemo from 'src/components/sitemap/Treeview';
+function Site() {
+  return (
+    <div>
+        {/* <Sitemap/> */}
+       <HandleExpansionDemo/>
+    </div>
+  )
+}
+
+export default Site;
